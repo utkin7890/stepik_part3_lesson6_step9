1be916fbcee7923bce48696eb35753f55746d77e
utkin-ivan <89164117260@mail.ru>
2022-11-28 19:27:46 +0300

    added file requirements.txt

--- conftest.py
+++ conftest.py
@@ -2,8 +2,18 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default= 'en-gb',
-                     help="Choose language: ru, fr, es, en-gb")
+from selenium.webdriver.chrome.options import Options
 
+
+
+@pytest.fixture(scope="function")
+def browser():
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
